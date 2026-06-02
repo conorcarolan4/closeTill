a98f1c1d335df26027515e18a8f0e74bdd03a444
conorcarolan4 <conorcarolan4@gmail.com>
2026-06-02 03:50:33 +0100

    Add files via upload
    Finished the base program and tested with one example test case. Still needs more testing as I feel like I haven't covered all test cases.

--- calculations.java
+++ calculations.java
@@ -1,55 +1,95 @@
+import java.util.Scanner;
+
 public class calculations 
 {
-    //credit
-    private double creditSales;
-    private double visaCard;
-    private double lottoPaidOut;
-    private double voucherSold;
-    private double voucherRedeemed;
-    private double driveOff;
-    private double delayedPayment;
-    private double shopPurchase;
-    private double outstandingPayment;
-    private double creditInternal;
+    Scanner scan = new Scanner(System.in);
 
-    //money in till
-    private double fiveC;
-    private double tenC;
-    private double twentyC;
-    private double fiftyC;
-    private double oneEuro;
-    private double twoEuro;
-    private double notes;
-
-    //cashdrop
-    private double cashDrop;
-
-    //reconciliation summary
-    private double closeFloatTill;
-    
-    //sales summary
-    private double fuelPrintOut;
-    private double shopPrintOut;
-
-    public double creditCalculation()
+    //calculates the credit section of the till closing procedure
+    public double creditCalculation(double creditSales, double visaCard, double lottoPaidOut, double voucherRedeemed, double voucherSold, 
+        double driveOff, double delayedPayment, double shopPurchase, double outstandingPayment)
     {
         double total = creditSales + visaCard + lottoPaidOut + voucherRedeemed + driveOff + delayedPayment + shopPurchase + outstandingPayment;
         total -= voucherSold;
         return total;
     }
 
-    public double calculateMoneyTotal()
+    //adds up all the different money values to give a total value
+    public double calculateMoneyTotal(double fiveC, double tenC, double twentyC, double fiftyC, double oneEuro, double twoEuro, double notes)
     {
         return fiveC + tenC + twentyC + fiftyC + oneEuro + twoEuro + notes;
     }
 
+    //calculates the float drop.
+    //float drop should be divisible by 5 and must always leave the till with 500 left over + coin cup ...
+    //... i decided to do that in the main as I thought it would be easier.
     public double calculateFloatDrop(double total)
     {
-        return total - 500.00;
+        return Math.floor(total / 5) * 5;
     }
 
+    //calculates coin cup.
+    //subtracts the total money from float drop to leave us with the spare change aka coin cup
+    public double calculateCoinCup(double total)
+    {
+        return total - calculateFloatDrop(total);
+    }
+
+    //calculates the total value of cash drops including the float drop.
+    //since there isnt a set number of cash drops per shift I decided to ask the user how many ...
+    //... cash drops they had in their shift and worked from there. not sure if thats the most efficient...
+    //... way to do it but its the way I came up with.
     public double cashDropTotalCalculation(double floatDrop)
     {
-        
+        System.out.println("Enter Number of Cash Drops: ");
+        int cashDropNum = scan.nextInt();
+        double cashDropTotal = 0;
+
+        if (cashDropNum > 0)
+        {
+            for (int i = 0; i < cashDropNum; i++)
+            {
+                cashDropTotal += scan.nextDouble();
+            }
+        }
+        else
+        {
+            cashDropNum = 0;
+            cashDropTotal = 0;
+        }
+
+        return cashDropTotal + floatDrop;
+    }
+
+    //adds up all the variables needed for the reconcilliation part of the close procedure.
+    //1885.00 is a set number for the till and will never change.
+    public double reconciliationSummary(double closeFloatTill, double cashDrop, double credit)
+    {
+        return 1885.00 + closeFloatTill + cashDrop + credit;
+    }
+
+    //adds up all the variables needed for the sales summary part of the close procedure.
+    //1885.00 and 500.00 are set values for the till that will never change.
+    public double salesSummary(double fuelPrintOut, double shopPrintOut)
+    {
+        return fuelPrintOut + shopPrintOut + 500.00 + 1885.00;
+    }
+
+    //this checks to see if the till is over or under.
+    //I used this opportunity to learn about formatting print statements to get the decimals correct.
+    public void shortOverCheck(double recSum, double salesSum)
+    {   
+
+        if (recSum > salesSum)
+        {
+            double over = recSum - salesSum;
+            over = Math.round(over * 100.0) / 100.0;
+            System.out.printf("Over: %.2f%n", over); 
+        }
+        else if (recSum < salesSum)
+        {
+            double under = salesSum - recSum;
+            under = Math.round(under * 100.0) / 100.0;
+            System.out.printf("Short: %.2f%n", under); 
+        }
     }
 }
--- closeTill.java
+++ closeTill.java
@@ -1,7 +1,103 @@
+import java.util.Scanner;
+
 public class closeTill
 {
     public static void main(String [] args)
     {
-        
+        calculations calculations = new calculations();
+        Scanner scan = new Scanner(System.in);
+
+        //variables needed for the calculations
+        double creditSales;
+        double visaCard;
+        double lottoPaidOut;
+        double voucherSold;
+        double voucherRedeemed;
+        double driveOff;
+        double delayedPayment;
+        double shopPurchase;
+        double outstandingPayment;
+        double creditInternal;
+        double fiveC;
+        double tenC;
+        double twentyC;
+        double fiftyC;
+        double oneEuro;
+        double twoEuro;
+        double notes;
+        double totalMoney;
+        double cashDrop;
+        double floatDrop;
+        double coinCup;
+        double closeFloatTill;
+        double recSum;
+        double salesSum;
+        double fuelPrintOut;
+        double shopPrintOut;
+
+        //asking the user for the information needed to ...
+        //... complete the calculations
+        System.out.println("Please enter below the following information.");
+        System.out.println("Credit Sales: ");
+        creditSales = scan.nextDouble();
+        System.out.println("Visa Card: ");
+        visaCard = scan.nextDouble();
+        System.out.println("Lotto Paid Out: ");
+        lottoPaidOut = scan.nextDouble();
+        System.out.println("Vouchers Sold: ");
+        voucherSold = scan.nextDouble();
+        System.out.println("Vouchers Redeemed: ");
+        voucherRedeemed = scan.nextDouble();
+        System.out.println("Drive Off: ");
+        driveOff = scan.nextDouble();
+        System.out.println("Delayed Payment: ");
+        delayedPayment = scan.nextDouble();
+        System.out.println("Shop Purchase: ");
+        shopPurchase = scan.nextDouble();
+        System.out.println("Outstanding Payment: ");
+        outstandingPayment = scan.nextDouble();
+        System.out.println("5c: ");
+        fiveC = scan.nextDouble();
+        System.out.println("10c: ");
+        tenC = scan.nextDouble();
+        System.out.println("20c: ");
+        twentyC = scan.nextDouble();
+        System.out.println("50c: ");
+        fiftyC = scan.nextDouble();
+        System.out.println("1 euro: ");
+        oneEuro = scan.nextDouble();
+        System.out.println("2 euro: ");
+        twoEuro = scan.nextDouble();
+        System.out.println("Notes: ");
+        notes = scan.nextDouble();
+        System.out.println("Fuel Print Out: ");
+        fuelPrintOut = scan.nextDouble();
+        System.out.println("Shop Print Out: ");
+        shopPrintOut = scan.nextDouble();
+
+        //instantiating variables with methods
+        creditInternal = calculations.creditCalculation(creditSales, visaCard, lottoPaidOut, voucherRedeemed, voucherSold, driveOff, delayedPayment, shopPurchase, outstandingPayment);
+        totalMoney =  calculations.calculateMoneyTotal(fiveC, tenC, twentyC, fiftyC, oneEuro, twoEuro, notes);
+        floatDrop = (calculations.calculateFloatDrop(totalMoney) - 500.00);
+        coinCup = calculations.calculateCoinCup(totalMoney);
+        cashDrop = calculations.cashDropTotalCalculation(floatDrop);
+        closeFloatTill = 500.00 + coinCup;
+        recSum = calculations.reconciliationSummary(closeFloatTill, cashDrop, creditInternal);
+        salesSum = calculations.salesSummary(fuelPrintOut, shopPrintOut);
+
+        //printing out final values for user to use.
+        System.out.println("Credit Internal: " + creditInternal);
+        System.out.println("Cash Drops: " + cashDrop);
+        System.out.println("Float Drop: " + floatDrop);
+        System.out.println("Total Money: " + totalMoney);
+        System.out.printf("Coin Cup: %.2f%n", coinCup);
+        System.out.printf("Close Float Till: %.2f%n", closeFloatTill);
+        System.out.println("Fuel Print Out: " + fuelPrintOut);
+        System.out.println("Shop Print Out: " + shopPrintOut);
+        System.out.println("Reconcilliation: " + recSum);
+        System.out.println("Sales Summary: " + salesSum);
+        calculations.shortOverCheck(recSum, salesSum);
+
+        scan.close();
     }
 }
